2947acb1c6984cbed17df8021d1147b45670d08d
neolao <contact@neolao.com>
2014-01-25 22:06:48 +0100

    Base of the bundle workflow

--- start.js
+++ start.js
@@ -1,5 +1,7 @@
 var solfege = require('./lib/solfege');
 var application = new solfege.kernel.Application(__dirname);
 
+application.addBundle(new solfege.bundle.server.HttpServer);
+
 application.start();
 
