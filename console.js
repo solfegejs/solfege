30a5fe4c43b46985922664e733077d2b790b3852
neolao <contact@neolao.com>
2014-01-30 23:51:05 +0100

    Ability to override the bundle configurations

--- console.js
+++ console.js
@@ -1,23 +1,19 @@
 var solfege = require('./lib/solfege');
+
+// Initialize the application
 var application = new solfege.kernel.Application(__dirname);
 
+// Add the bundles
 application.addBundle('server', new solfege.bundle.server.HttpServer);
 application.addBundle('console', new solfege.bundle.cli.Console);
 
+// Override configuration
+application.overrideConfiguration({
+    server: {
+        port: 7777
+    }
+});
+
+// Start the application
 application.start();
 
-
-/*
-
-// Add the commands from the bundles
-var bundles = application.getBundles();
-for (var bundleIndex in bundles) {
-    var bundle = bundles[bundleIndex];
-    var bundleCommands = bundle.getConsoleCommands();
-    cli.addCommands(bundle.getName(), bundleCommands);
-}
-
-// Run the command line inteface
-var input = new ArgvInput();
-cli.run(input);
-*/
