0183abead8135fb5d7b3243813d390bbdb737c9d
neolao <contact@neolao.com>
2014-01-31 21:56:53 +0100

    Remove the bundles

--- start.js
+++
@@ -1,7 +0,0 @@
-var solfege = require('./lib/solfege');
-var application = new solfege.kernel.Application(__dirname);
-
-application.addBundle('server', new solfege.bundle.server.HttpServer);
-
-application.start();
-
